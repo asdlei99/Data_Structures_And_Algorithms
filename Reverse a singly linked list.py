3fb550d2575c39c9c2dbb38866b53045fe0f36e4
Ying Joy <yzk.1314@outlook.com>
2018-04-19 18:35:50 +0800

    algorithms

---
+++ Reverse a singly linked list.py
@@ -0,0 +1,28 @@
+#!usr/bin/env python  
+#-*- coding:utf-8 -*- 
+
+""" 
+@author:yzk13 
+@time: 2018/04/19
+Reverse a singly linked list.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre = None
+        while head:
+            cursor = head
+            head = head.next
+            cursor.next = pre
+            pre = cursor
+        return pre
